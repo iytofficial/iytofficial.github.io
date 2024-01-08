be881dc581332e08cc24b71d190ce290057af668
iytofficial <shihabmuhtasim@gmail.com>
2024-01-09 01:17:31 +0800

    feedback update2.1

--- index_php.php
+++ index_php.php
@@ -224,10 +224,7 @@
             <div class="col-md-12">
                <div id="myCarousel" class="carousel slide" data-ride="carousel">
                   <ol class="carousel-indicators">
-                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                     <li data-target="#myCarousel" data-slide-to="1"></li>
-                     <li data-target="#myCarousel" data-slide-to="2"></li>
-                     <li data-target="#myCarousel" data-slide-to="3"></li>
+                     
                   </ol>
                   <div class="carousel-inner">
                      <div class="carousel-item active">
